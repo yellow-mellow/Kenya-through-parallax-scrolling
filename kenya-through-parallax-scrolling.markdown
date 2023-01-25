0c87e1ee54ea50250cb764c6cf38bab5e7151f3a
Michelle <79482443+yellow-mellow@users.noreply.github.com>
2023-01-25 05:52:21 +0100

    Add files via upload

---
+++ kenya-through-parallax-scrolling.markdown
@@ -0,0 +1,7 @@
+Kenya through Parallax Scrolling
+--------------------------------
+This is a fun website that highlights some interesting facts about my country, Kenya.The site was coded using HTML, CSS and Javascript. It uses Parallax scrolling as an intersting way to engage the users as they read through the contents of the webpage. 
+
+A [Pen](https://codepen.io/yellow-mellow/pen/rNrWGVz) by [Michelle](https://codepen.io/yellow-mellow) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/rNrWGVz).
---
+++ script.js
@@ -0,0 +1,36 @@
+$('.img-parallax').each(function(){
+  var img = $(this);
+  var imgParent = $(this).parent();
+  function parallaxImg () {
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
+
+
+          
+    var winBottom = winY + winH;
+
+    
+    if (winBottom > imgY && winY < imgY + parentH) {
+      
+      var imgBottom = ((winBottom - imgY) * speed);
+     
+      var imgTop = winH + parentH;
+     
+      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+    }
+    img.css({
+      top: imgPercent + '%',
+      transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+  }
+  $(document).on({
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
+    }
+  });
+});
